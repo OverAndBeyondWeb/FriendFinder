4d697f6873b8959cb98bd1e1e6dae2a489609d79
Juan Fox <foxj0875@gmail.com>
2018-04-01 14:56:52 -0400

    express routes for home.html and survey.html working

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var PORT = process.env.PORT || 3000;
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/app/public/home.html');
+});
+
+app.get('/survey', function(req, res) {
+  res.sendFile(__dirname + '/app/public/survey.html');
+});
+
+
+
+
+app.listen(PORT, function() {
+  console.log('listening on port: ' + PORT)
+});
