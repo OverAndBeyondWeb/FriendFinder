43f0357eabf64bd875725cf69012020648900e44
Juan Fox <foxj0875@gmail.com>
2018-04-03 14:08:43 -0400

    scores from user questions are subtracted from existing user scores and match is found

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,6 +7,24 @@ function runAPI(app, data) {
     res.json(data);
   })
   .post(function(req, res) {
+    var newFriend = req.body;
+  
+    var friendsAndScores = friends.map(function(friend, index) {
+     
+      var accumalitiveScore = friend.scores.reduce(function(acc, next, currentIndex) {
+        var newFriendScore = parseInt(newFriend.scores[currentIndex]);
+        return acc + Math.abs(next - newFriendScore);
+      }, 0);
+      
+      return {name: friend.name, accScore: accumalitiveScore};
+    });
+
+    var sortedFriendsAndScores = friendsAndScores.sort(function(a, b) {
+      return a.accScore - b.accScore;
+    });
+
+    var match = sortedFriendsAndScores[0];
+
     friends.push(req.body);
   });
 }
